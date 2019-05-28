a1092a2fa491357a115ca4f46748c25f351a8554
ashwin9949 <51076815+ashwin9949@users.noreply.github.com>
2019-05-28 08:34:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,10 @@ echo "testing" >> test
 ls -ltr
 echo "$Environment for $Variable" >> test
 
+name="Ashwin"
+
+echo $name
+
 echo "[INFO] GIT Jenkins Integratioon Demo"
 echo "[INFO] POLL SCM Demo"
 echo "[INFO] POLL SCM Demo to check changes"
